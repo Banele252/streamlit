15a2a3d0d7b0da42632bcc03426efdf59f8ebdef
Banele Mdluli <banele.mdluli25@gmail.com>
2025-10-15 06:33:12 +0200

    changes made

---
+++ section_2/model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from joblib import dump
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
+from sklearn.ensemble import GradientBoostingClassifier
+
+URL = r"C:\Users\Keith\OneDrive - University of Johannesburg\Documents\Data scientist\Streamlit\section_2\mushrooms.csv"
+COLS = ['class', 'odor', 'gill-size', 'gill-color', 'stalk-surface-above-ring',
+       'stalk-surface-below-ring', 'stalk-color-above-ring',
+       'stalk-color-below-ring', 'ring-type', 'spore-print-color']
+
+# Read data
+df = pd.read_csv(URL)
+df = df[COLS]
+
+# Create pipeline
+pipe = Pipeline([
+    ('feature_encoder', OrdinalEncoder()),
+    ('target_encoder', LabelEncoder()),
+    ('gbc', GradientBoostingClassifier(max_depth=5, random_state=42))
+])
+
+# Fit the pipeline
+X = df.drop(['class'], axis=1)
+y = df['class']
+
+X = pipe.named_steps['feature_encoder'].fit_transform(X)
+y = pipe.named_steps['target_encoder'].fit_transform(y)
+pipe.named_steps['gbc'].fit(X, y)
+
+# Save the pipeline
+dump(pipe, 'section_2/mushroom_model.joblib')
---
+++ section_2/playground.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Keith\OneDrive - University of Johannesburg\Documents\Data scientist\Streamlit\section_2\mushrooms.csv")
