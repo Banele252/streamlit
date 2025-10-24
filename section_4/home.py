32cdaa5bc413698f926998e1235abf0791274025
Banele Mdluli <banele.mdluli25@gmail.com>
2025-10-24 19:57:28 +0200

    add multi channel state

--- section_4/home.py
+++ section_4/home.py
@@ -12,16 +12,27 @@ st.set_page_config(
 # Initialize a sample df and store it in the session state
 df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
 
-if "df" not in st.session_state:
-    st.session_state['df'] = df
-# Initialize state with the key "product" set to 0
-if "product" not in st.session_state:
-    st.session_state['product'] = 0
+if all(key not in st.session_state.keys() for key in ("product", "x1", "x2")):
+    st.session_state["product"] = 0
+    st.session_state["x1"] = 0 
+    st.session_state["x2"] = 0
+
+st.session_state["df"] = df
+
+
+
 
 
 # Define a function to multiply two numbers
-def multiply(x, y):
+def multiply(x1, x2):
     st.session_state['product'] = x2 * x1
+
+def keep(key):
+    st.session_state[key] = st.session_state[f"_{key}"]
+
+
+def unkeep(key):
+    st.session_state[f"_{key}"] = st.session_state[key]
 
 
 
@@ -34,9 +45,11 @@ if __name__ == "__main__":
     col1, col2 = st.columns(2)
 
     with col1:
-        x1 = st.number_input("Pick a number", min_value=0, max_value=10, key="x1")
+        unkeep('x1')
+        x1 = st.number_input("Pick a number", min_value=0, max_value=10, key="_x1", on_change=keep, args=("x1",))
     with col2:
-        x2 = st.number_input("Pick another number", min_value=0, max_value=10, key="x2")
+        unkeep('x2')
+        x2 = st.number_input("Pick another number", min_value=0, max_value=10, key="_x2", on_change=keep, args=("x2",))
 
     st.button("Multiply!", type="primary", on_click=multiply, args=(x1, x2))
 
